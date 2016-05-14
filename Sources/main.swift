afa5bcaa2de1707f7b2074d35b4bad9b6e0baf88
Larry Fox <larrywfox@gmail.com>
2016-05-14 15:25:45 -0400

    Make watch pipe friendly

--- Sources/main.swift
+++ Sources/main.swift
@@ -1,31 +1,37 @@
 import Foundation
 
-guard Process.arguments.count >= 3 else {
-    print("usage: watch <path> <cmd>...")
+signal(SIGINT) { exit($0) }
+
+let ARGV = Process.arguments
+
+var separator = ":"
+var offset = 1
+
+if ARGV.count > offset && ARGV[1] == "-n" {
+    separator = "\n"
+    offset = 2
+}
+
+guard ARGV.count > offset else {
+    fputs("usage: watch [-n] <path> [<command>]", stderr)
     exit(1)
 }
 
-signal(SIGINT) { exit($0) }
-
-let path = Process.arguments[1]
-let cmd = Array(Process.arguments[2..<Process.arguments.count])
+let path = ARGV[offset]
+let cmd = Array(ARGV[(1 + offset)..<ARGV.count])
+let hasCmd = !cmd.isEmpty
 let dir = cwd()
-let statuses = Chan<Int32>()
 let stream = FSEventStream(paths: [path])
-
-go {
-    for status in statuses {
-        if status != 0 {
-            print(" failed : [\(status)] \(cmd.joined(separator: " "))")
-        }
-    }
-}
 
 for events in stream {
     let paths = events
         .map { $0.path.removePrefix("\(dir)/") }
-        .joined(separator: ", ")
+        .joined(separator: separator)
 
-    print("changed : \(paths)")
-    go { statuses <- exec(cmd) }
+    fputs("\(paths)\n", stdout)
+    fflush(stdout)
+
+    if hasCmd {
+        go { exec(cmd) }
+    }
 }
